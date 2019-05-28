54751984383901c0bdb978a0a612acfd1d3c4bae
LemonCoo1 <13545141090@163.com>
2019-05-28 16:37:03 +0800

    Initial commit 集成Zxing

---
+++ app/src/main/java/com/forest/npumpcon/MainActivity.java
@@ -0,0 +1,22 @@
+package com.forest.npumpcon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.forest.npumpcon.zxing.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openZxing(View view) {
+        Intent intent = new Intent(this, CaptureActivity.class);
+        startActivity(intent);
+    }
+}
